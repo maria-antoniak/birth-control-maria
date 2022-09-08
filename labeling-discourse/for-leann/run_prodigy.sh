530d5862b8fc485deee06d1998770de71ac1efc6
maria-antoniak <maa343@cornell.edu>
2022-09-08 00:15:38 -0700

    ...

--- labeling-discourse/for-leann/run_prodigy.sh
+++ labeling-discourse/for-leann/run_prodigy.sh
@@ -6,10 +6,10 @@
 DATA_DIRECTORY_PATH="/Users/maria/Documents/data/birth-control/labeling/label-discourse"
 
 # ALL labels at a time for REDDIT POSTS
-prodigy textcat.manual discourse-reddit-posts $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.reddit-posts.jsonl --label labels.txt
+# prodigy textcat.manual discourse-reddit-posts $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.reddit-posts.jsonl --label labels.txt
 
 # ALL labels at a time for REDDIT COMMENTS
-# prodigy textcat.manual discourse-reddit-comments $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.reddit-comments.jsonl --label labels.txt
+prodigy textcat.manual discourse-reddit-comments $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.reddit-comments.jsonl --label labels.txt
 
 # ALL labels at a time for TWITTER POSTS
 # prodigy textcat.manual discourse-twitter-posts $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.twitter-posts.jsonl --label labels.txt
--- labeling-discourse/run_prodigy.sh
+++ labeling-discourse/run_prodigy.sh
@@ -9,7 +9,7 @@ DATA_DIRECTORY_PATH="/Users/maria/Documents/data/birth-control/labeling/label-di
 # prodigy textcat.manual discourse-reddit-posts $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.reddit-posts.jsonl --label labels.txt
 
 # ALL labels at a time for REDDIT COMMENTS
-# prodigy textcat.manual discourse-reddit-comments $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.reddit-comments.jsonl --label labels.txt
+prodigy textcat.manual discourse-reddit-comments $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.reddit-comments.jsonl --label labels.txt
 
 # ALL labels at a time for TWITTER POSTS
 # prodigy textcat.manual discourse-twitter-posts $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.twitter-posts.jsonl --label labels.txt
@@ -18,4 +18,4 @@ DATA_DIRECTORY_PATH="/Users/maria/Documents/data/birth-control/labeling/label-di
 # prodigy textcat.manual discourse-twitter-replies $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.twitter-replies.jsonl --label labels.txt
 
 # ALL labels at a time for WEBMD
-prodigy textcat.manual discourse-webmd-reviews $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.webmd-reviews.jsonl --label labels.txt
+# prodigy textcat.manual discourse-webmd-reviews $DATA_DIRECTORY_PATH/sampled-sentences.prodigy.webmd-reviews.jsonl --label labels.txt
